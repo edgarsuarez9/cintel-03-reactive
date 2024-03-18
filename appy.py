b62841cb02327fbef95048b2c53469d737ea743b
edgarsuarez9 <161646982+edgarsuarez9@users.noreply.github.com>
2024-03-18 17:19:19 -0500

    Update appy.py

--- appy.py
+++ appy.py
@@ -91,3 +91,15 @@ with ui.accordion():
             ax.set_xlabel("Mass")
             ax.set_ylabel("Count")
             return ax
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+@reactive.calc
+def filtered_data():
+    return penguins_df
